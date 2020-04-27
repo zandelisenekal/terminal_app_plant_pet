6436c062c8cdcf1f12293797836137131f478cc2
zandeli <zandelisenekal@gmail.com>
2020-04-27 17:36:11 +1000

    made app executable

---
+++ src/run_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install dependencies from Gemfile
+# bundle install
+
+# run application
+ruby new_game.rb
+
+# instructions to run app
+# open terminal in bash and type in command line: bash run_app.sh
+# will open game
